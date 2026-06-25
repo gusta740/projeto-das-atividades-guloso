7106888f388eea92a131660a018828490e73ea79
jordanacunha <jordana.cunha@escola.pr.gov.br>
2026-06-25 12:14:44 -0300

    front e back

---
+++ front e backend/atividade 10/script.js
@@ -0,0 +1,14 @@
+console.log("Servidor iniciado com Node.js");
+
+console.log("Meu nome é Gustavo");
+
+console.log("Projeto Escolar");
+
+console.log("Sistema carregado com sucesso.");
+console.log("Conexão realizada.");
+console.log("Aguardando usuários.");
+
+console.log("Olá equipe! Bem-vindos ao projeto escolar.");
+
+console.log("Teste iniciado.");
+console.log("Teste concluído.");
